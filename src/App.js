fb0d9df24fb5c9372ff4becfdb3e3fc3d16f3a5a
simply-code-it <dashwar.pradeep@gmail.com>
2024-05-12 10:08:29 +0530

    added functionality for mergesort

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer.jsx';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <SortingVisualizer></SortingVisualizer>
     </div>
   );
 }
 
 export default App;
+
+
+// <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
